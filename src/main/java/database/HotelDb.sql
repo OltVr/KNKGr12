68797bff6724157cd25f62b05e92457d5a678198
Olta Olloni <oltaxx@gmail.com>
2024-05-07 15:31:33 +0200

    Edited database

--- src/main/java/database/HotelDb.sql
+++ src/main/java/database/HotelDb.sql
@@ -1,19 +1,22 @@
 create database Hotel;
-
 use Hotel;
 
--- me u shtu check constraints per email, pass.
+-- nese nuk ju ekzekutohet diqka bojeni drop edhe create prap
+drop database Hotel;
+
+
+-- me u shtu check constraints per email, pass (+qysh me e lidh qe me dal si alert per user)
 CREATE TABLE IF NOT EXISTS user (
-email varchar(255) not null primary key,
-firstName varchar(255) NOT NULL,
-lastName varchar(255) NOT NULL,
-salt varchar(50) NOT NULL,
-passwordHash varchar(255) not Null,
-isAdmin boolean NOT NULL DEFAULT 0,
-CreatedAt datetime NOT NULL default NOW()
+    email varchar(255) not null primary key,
+    firstName varchar(255) NOT NULL,
+    lastName varchar(255) NOT NULL,
+    salt varchar(50) NOT NULL,
+    passwordHash varchar(255) not Null,
+    isAdmin boolean NOT NULL DEFAULT 0,
+    CreatedAt datetime NOT NULL default NOW()
 );
 
--- me u shtu check constraints per roomNumber, floorNumber, capacity, bedNumber, price.
+-- me u shtu check constraints per roomNumber, floorNumber, capacity, bedNumber, price (+qysh me e lidh qe me dal si alert per user)
 CREATE TABLE IF NOT EXISTS room (
     roomNumber INT NOT NULL PRIMARY KEY,
     floorNumber INT NOT NULL,
