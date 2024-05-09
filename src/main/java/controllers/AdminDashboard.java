62e6a1017aa4377c675bf199d8311f464591b4f4
Olt Vrella <oltvrella@gmail.com>
2024-05-09 13:36:14 +0200

    Functionalized admin dashboard and room buttons on the side panel

--- src/main/java/controllers/AdminDashboard.java
+++ src/main/java/controllers/AdminDashboard.java
@@ -1,4 +1,32 @@
 package controllers;
 
+import App.Navigator;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class AdminDashboard {
+    @FXML
+    private AnchorPane dashboardPane;
+
+    @FXML
+    private AnchorPane roomPane;
+
+    @FXML
+    private void handleLogout(MouseEvent me) {
+        Navigator.navigate(me,Navigator.LOGIN_PAGE);
+    }
+
+    @FXML
+    private void handleDashboard(ActionEvent ae){
+        dashboardPane.setVisible(true);
+        roomPane.setVisible(false);
+    }
+
+    @FXML
+    private void handleRooms(ActionEvent ae){
+        dashboardPane.setVisible(false);
+        roomPane.setVisible(true);
+    }
 }
