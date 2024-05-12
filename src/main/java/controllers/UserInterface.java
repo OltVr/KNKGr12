5227f5d6fcb9d70437f48f7c72125bb2f4b06000
oraosmanii <oraosmani@gmail.com>
2024-05-13 01:46:11 +0200

    funksionalizimi i navigimit neper menyte e adminit

--- src/main/java/controllers/UserInterface.java
+++ src/main/java/controllers/UserInterface.java
@@ -5,12 +5,38 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 
 public class UserInterface implements Initializable {
+    @FXML
+    private AnchorPane homePane;
+    @FXML
+    private AnchorPane reservationsPane;
+    @FXML
+    private AnchorPane historyPane;
+
+    @FXML
+    private void handleHome(){
+        homePane.setVisible(true);
+        reservationsPane.setVisible(false);
+        historyPane.setVisible(false);
+    }
+    @FXML
+    private void handleReservations(){
+        homePane.setVisible(false);
+        reservationsPane.setVisible(true);
+        historyPane.setVisible(false);
+    }
+    @FXML
+    private void handleHistory(){
+        homePane.setVisible(false);
+        reservationsPane.setVisible(false);
+        historyPane.setVisible(true);
+    }
 
     @FXML
     private void handleLogOut(ActionEvent ae) {
