f1cf63de054b2a6f223fe4a4e324578c7ae224b9
BrinetaUka <brineta.uka@student.uni-pr.edu>
2024-05-13 14:41:09 +0200

    Update UserInterface.java

--- src/main/java/controllers/UserInterface.java
+++ src/main/java/controllers/UserInterface.java
@@ -5,6 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
@@ -18,36 +19,59 @@ public class UserInterface implements Initializable {
     private AnchorPane reservationsPane;
     @FXML
     private AnchorPane historyPane;
-
+    @FXML
+    private AnchorPane seaViewPane;
+    @FXML
+    private AnchorPane cityViewPane;
     @FXML
     private void handleHome(){
         homePane.setVisible(true);
         reservationsPane.setVisible(false);
         historyPane.setVisible(false);
+        seaViewPane.setVisible(false);
+        cityViewPane.setVisible(false);
     }
     @FXML
     private void handleReservations(){
         homePane.setVisible(false);
         reservationsPane.setVisible(true);
         historyPane.setVisible(false);
-
+        seaViewPane.setVisible(false);
+        cityViewPane.setVisible(false);
     }
     @FXML
     private void handleHistory(){
         homePane.setVisible(false);
         reservationsPane.setVisible(false);
         historyPane.setVisible(true);
+        seaViewPane.setVisible(false);
+        cityViewPane.setVisible(false);
     }
 
     @FXML
     private void handleLogOut(ActionEvent ae) {
         Navigator.navigate(ae,Navigator.LOGIN_PAGE);
 
-
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+    @FXML
+    public void handleCheckSeaView(MouseEvent me){
+        homePane.setVisible(false);
+        reservationsPane.setVisible(false);
+        historyPane.setVisible(false);
+        seaViewPane.setVisible(true);
+        cityViewPane.setVisible(false);
+    }
+    @FXML
+    public void handleCheckCityView(MouseEvent me){
+        homePane.setVisible(false);
+        reservationsPane.setVisible(false);
+        historyPane.setVisible(false);
+        seaViewPane.setVisible(false);
+        cityViewPane.setVisible(true);
     }
 }
