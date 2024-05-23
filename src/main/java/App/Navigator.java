749ff687191423260db526e6bf844feca91a193f
Olta Olloni <oltaxx@gmail.com>
2024-05-24 00:26:38 +0200

    Added pop-ups for any signup error and fixed login popup

--- src/main/java/App/Navigator.java
+++ src/main/java/App/Navigator.java
@@ -16,8 +16,6 @@ public class Navigator {
     public final static String LOGIN_PAGE = "login_form.fxml";
     public final static String HOME_PAGE = "user_pages.fxml";
     public final static String CREATE_ACCOUNT_PAGE = "signup_form.fxml";
-    //FIXME: a duhet mu hek qeky loginFailAlert
-    public final static String LOGIN_FAIL_ALERT = "login_failed.fxml";
     public final static String ADMIN_DASHBOARD = "admin_pages.fxml";
     public final static String PROCEEDING_PAGE = "reserve_form.fxml";
     private static ResourceBundle bundle;
